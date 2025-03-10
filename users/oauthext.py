162d195d1e2eb78fea2fa2438618ddffe12b49b0
Saco Bekius <saco@hoogekamp.nl>
2025-03-10 08:29:53 +0100

    username claim toevoegen

--- users/oauthext.py
+++ users/oauthext.py
@@ -11,6 +11,7 @@ class CustomOAuth2Validator(OAuth2Validator):
 
     def get_userinfo_claims(self, request):
         claims = super().get_userinfo_claims(request)
+        claims['username'] = request.user.email
         claims['email'] = request.user.email
         claims['givenName'] = request.user.first_name,
         claims['surname'] = request.user.last_name,
