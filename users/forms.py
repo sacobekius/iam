0595df806f6c931d7f9cd72a5a66e444091afab3
Saco Bekius <saco@hoogekamp.nl>
2025-06-03 15:21:52 +0200

    Refactor css en layout

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,9 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'groups')
+        widgets = {
+            'groups': forms.CheckboxSelectMultiple,
+        }
         help_texts = {
             'username': None,
             'groups': 'Selecteer meerdere groepen met Ctrl op MS-Windows en Cmd op Mac OS'
